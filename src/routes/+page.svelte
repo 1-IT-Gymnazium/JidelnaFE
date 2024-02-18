ac376d2314ea29bfb4f2ddd5c0d96a94334ec241
vendeliin <vendelinkukla@gmial.com>
2024-02-18 15:53:00 +0100

    Stanka se nonstop kouka po inputu, simuluje cekanni na nacteni isicu. Kdyz klinu spravne cislo id, tak mi to posle spravna data.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,48 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import {onDestroy, onMount} from 'svelte'
+	import {browser} from '$app/environment'
+
+	let name = ''
+	let lunch = ''
+
+	const load = async (id) => {
+		try {
+			const response = await fetch(`http://127.0.0.1:8000/users/${id}`)
+			return await response.json()
+		} catch (error) {
+			console.error('Error fetching data:', error)
+			return null
+		}
+	}
+
+	onMount(() => {
+		if(!browser) return
+		window.addEventListener('keydown', handeKeyDown)
+	})
+
+	onDestroy(() =>{
+		if(!browser) return
+		window.removeEventListener('keydown', handeKeyDown)
+	})
+
+	const handeKeyDown = async (e) => {
+		console.log(e.key)
+		const data = await load(e.key)
+		if (data) {
+			// If data is retrieved successfully, update the name variable
+			name = data.name;
+			lunch = data.lunches[0].type_of_lunch
+
+		} else {
+			name = ''
+			lunch = ''
+		}
+	}
+</script>
+
+
+
+<div>
+	<h1>Jmeno: {name}</h1>
+	<h2>Cislo obeda: {lunch}</h2>
+</div>
