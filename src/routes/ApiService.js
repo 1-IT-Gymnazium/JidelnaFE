34d152292307bb12b5420504d6d226a9a4c04c3b
vendeliin <vendelinkukla@gmial.com>
2024-03-12 21:14:10 +0100

    admin page funkcni

--- src/routes/ApiService.js
+++ src/routes/ApiService.js
@@ -28,13 +28,18 @@ export const loadUsersWithLunchOut = async () => {
 	}
 }
 
+
 export const uploadUser = async (id, name) => {
 	try {
-		const response = await fetch(`http://127.0.0.1:8000/create/user/${id}/${name}`)
+		const response = await fetch(`http://127.0.0.1:8000/create`, {method: 'POST',
+			headers: {
+				'accept' : 'application/json',
+				'Content-Type': 'application/json'
+			}, body: JSON.stringify({id: id, name:name, lunches:[]})
+		})
 		return await response.json()
 	} catch (error) {
 		console.error('Error fetching data:', error)
 		return null
 	}
 }
-
